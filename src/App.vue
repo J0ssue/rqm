2376a246d558fb49f9869c80cf35e3fb6321e35b
Josue Diaz <jdiaz77711@gmail.com>
2019-10-13 13:21:57 +0100

    finish rqm

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app" class="h-full flex justify-center items-center">
+		<main id="quote-box" class="bg-green-500 p-16 rounded">
+			<blockquote
+				id="text"
+				class="bg-gray-200 p-6 rounded"
+				v-text="rQuote.content"
+			></blockquote>
+			<p id="author" class="font-bold text-right mt-2 mb-6">
+				author: {{ rQuote.author }}
+			</p>
+			<div class="flex justify-between">
+				<a
+					:href="`https://twitter.com/intent/tweet?text=${rQuote.content}`"
+					target="_blank"
+					class="twitter-share-button bg-blue-500 inline-block text-center text-white uppercase rounded px-6 py-2 hover:bg-blue-600"
+					id="tweet-quote"
+				>
+					Tweet quote</a
+				>
+				<button
+					id="new-quote"
+					class="px-6 py-2 rounded bg-black text-white uppercase hover:bg-gray-900"
+					@click="getRandomQuote"
+				>
+					new quote
+				</button>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+	export default {
+		name: "app",
+		data() {
+			return {
+				rQuote: {}
+			};
+		},
+		mounted() {
+			fetch("https://api.quotable.io/random")
+				.then(res => {
+					return res.json();
+				})
+				.then(response => {
+					this.rQuote = response;
+				});
+		},
+		methods: {
+			getRandomQuote() {
+				fetch("https://api.quotable.io/random")
+					.then(res => {
+						return res.json();
+					})
+					.then(response => {
+						this.rQuote = response;
+					});
+			}
+		}
+	};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
